e99d5532b594740cb2c8fedd6e6ad314f2168882
Mgamerz <mgamerz2@gmail.com>
2017-06-04 16:01:19 -0600

    Added release builder script, MEM update

--- AlotAddOnGUI/Properties/AssemblyInfo.cs
+++ AlotAddOnGUI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.2.7.46")]
-[assembly: AssemblyFileVersion("1.2.7.46")]
+[assembly: AssemblyVersion("1.2.22.61")]
+[assembly: AssemblyFileVersion("1.2.22.61")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
---
+++ ReleaseBuilder.ps1
@@ -0,0 +1,8 @@
+﻿get-childitem "$($PSScriptRoot)\Release\" -include *.7z -recurse | foreach ($_) {remove-item $_.fullname}
+
+$fileversion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("Release\AlotAddonBuilder.exe").FileVersion
+$outputfile = "$($PSScriptRoot)\Release\ALOTAddonBuilder_$($fileversion).7z"
+$exe = "$($PSScriptRoot)\Release\\bin\7z.exe"
+$arguments = "a", "`"$($outputfile)`"", "`"$($PSScriptRoot)\Release\*`"", "-mmt6"
+Write-Host "Running: $($exe) $($arguments)"
+Start-Process $exe -ArgumentList $arguments -Wait -NoNewWindow
