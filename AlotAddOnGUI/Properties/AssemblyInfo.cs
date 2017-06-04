260e8c0addf93d36b0bb1c2ba641219f039f7e14
Mgamerz <mgamerz2@gmail.com>
2017-06-04 14:43:05 -0600

    More portable build steps

--- AlotAddOnGUI/Properties/AssemblyInfo.cs
+++ AlotAddOnGUI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.2.6.45")]
-[assembly: AssemblyFileVersion("1.2.6.45")]
+[assembly: AssemblyVersion("1.2.7.46")]
+[assembly: AssemblyFileVersion("1.2.7.46")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
