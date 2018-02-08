470c7f44a2b71a5250e90a26b468fd830c0e4b1a
Mgamerz <mgamerz2@gmail.com>
2018-02-07 17:18:10 -0700

    Missing addon files now has stronger worded recommendation

--- AlotAddOnGUI/MainWindow.xaml.cs
+++ AlotAddOnGUI/MainWindow.xaml.cs
@@ -894,6 +894,7 @@ namespace AlotAddOnGUI
 
                     if (af.Ready != ready) //status is changing
                     {
+                        Log.Information(af.FriendlyName + " changing ready states. Is now ready: " + ready);
                         af.ReadyStatusText = null;
                         af.ReadyIconPath = null;
                         af.Ready = ready;
@@ -2478,7 +2479,7 @@ namespace AlotAddOnGUI
             //if alot is already installed we don't need to show missing message, unless installed via MEM directly
             if (installedInfo == null || installedInfo.ALOTVER == 0)
             {
-                MessageDialogResult result = await this.ShowMessageAsync(nummissing + " file" + (nummissing != 1 ? "s are" : " is") + " missing", "Some files for the Mass Effect" + getGameNumberSuffix(game) + " Addon are missing - do you want to build the addon without these files?", MessageDialogStyle.AffirmativeAndNegative);
+                MessageDialogResult result = await this.ShowMessageAsync(nummissing + " file" + (nummissing != 1 ? "s are" : " is") + " missing", "Some files for the Mass Effect" + getGameNumberSuffix(game) + " addon are missing. These files add a significant amount of high quality textures from third party artists. These should be installed if you want all of the high quality textures; these files are not included directly in ALOT because of ownership rights.\n\nAre you sure you want to build the addon without these files?", MessageDialogStyle.AffirmativeAndNegative);
                 return result == MessageDialogResult.Affirmative;
             }
             else
