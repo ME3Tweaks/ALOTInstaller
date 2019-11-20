0ebe0032c05e7ef49d2e571770eba23f9d58eb85
Mgamerz <mgamerz2@gmail.com>
2019-11-19 23:30:49 -0700

    Update version info. Re-add assemblyinfo to repo since autonumbering has been disabled.

---
+++ AlotAddOnGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information
+
+[assembly: AssemblyTitle("ALOT Installer")]
+[assembly: AssemblyDescription("Installs ALOT for the Mass Effect Trilogy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ME3Tweaks")]
+[assembly: AssemblyProduct("ALOT Installer")]
+[assembly: AssemblyCopyright("Copyright © 2017 - 2019 ME3Tweaks")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version informationr(
+[assembly: AssemblyVersion("3.0.570.5548")]
+[assembly: AssemblyFileVersion("3.0.579.5548")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
