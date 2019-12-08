9018d65d3af3dc64431abba3c217e33d97fd1d89
Mgamerz <mgamerz2@gmail.com>
2019-12-08 15:59:57 -0700

    Bump version

--- AlotAddOnGUI/Properties/AssemblyInfo.cs
+++ AlotAddOnGUI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("3.0.572.5555")]
-[assembly: AssemblyFileVersion("3.0.572.5555")]
+[assembly: AssemblyVersion("3.0.573.5557")]
+[assembly: AssemblyFileVersion("3.0.573.5557")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
