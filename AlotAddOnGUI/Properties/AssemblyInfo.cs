6eda50e5e3143b8ec16285a6e6eb850c58037f7b
Mgamerz <mgamerz2@gmail.com>
2020-01-25 08:30:44 -0700

    Bumpb version

--- AlotAddOnGUI/Properties/AssemblyInfo.cs
+++ AlotAddOnGUI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("3.0.577.5584")]
-[assembly: AssemblyFileVersion("3.0.577.5584")]
+[assembly: AssemblyVersion("3.0.578.5588")]
+[assembly: AssemblyFileVersion("3.0.578.5588")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
