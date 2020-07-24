fa8ab99c53f35bb642e9c44f098ab66ce30d2386
Mgamerz <mgamerz2@gmail.com>
2020-07-24 17:36:33 -0600

    Sync commit: Add new version of compression wrappers dll

---
+++ ALOTInstallerCore/Builder/StageStep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ALOTInstallerCore.Objects;
+
+namespace ALOTInstallerCore.Builder
+{
+    // Extraction + Staging <<
+    // Building
+    // Installing
+
+
+    /// <summary>
+    /// Object that handles the staging step of texture package building
+    /// </summary>
+    public class StageStep
+    {
+
+        private void ExtractFile(InstallerFile instFile)
+        {
+            if (instFile is ManifestFile mf)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ ALOTInstallerCore/Helpers/MEMIPCHandler.cs
@@ -0,0 +1,55 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ALOTInstallerCore.Helpers
+{
+    public static class MEMIPCHandler
+    {
+        public static void RunMassEffectModderNoGuiIPC(string operationName, string exe, string args, object lockObject, Action<string, string> exceptionOccuredCallback, Action<int?> setExitCodeCallback = null, Action<string, string> ipcCallback = null)
+        {
+            Log.Information($@"Running Mass Effect Modder No GUI w/ IPC: {exe} {args}");
+            var memProcess = new ConsoleApp(exe, args);
+            bool hasExceptionOccured = false;
+            memProcess.ConsoleOutput += (o, args2) =>
+            {
+                string str = args2.Line;
+                if (hasExceptionOccured)
+                {
+                    Log.Fatal(@"MassEffectModderNoGui.exe: " + str);
+                }
+                if (str.StartsWith(@"[IPC]", StringComparison.Ordinal))
+                {
+                    string command = str.Substring(5);
+                    int endOfCommand = command.IndexOf(' ');
+                    if (endOfCommand >= 0)
+                    {
+                        command = command.Substring(0, endOfCommand);
+                    }
+
+                    string param = str.Substring(endOfCommand + 5).Trim();
+                    if (command == @"EXCEPTION_OCCURRED")
+                    {
+                        hasExceptionOccured = true;
+                        exceptionOccuredCallback?.Invoke(operationName, param);
+                        return; //don't process this command further, nothing handles it.
+                    }
+
+                    ipcCallback?.Invoke(command, param);
+                }
+                //Debug.WriteLine(args2.Line);
+            };
+            memProcess.Exited += (a, b) =>
+            {
+                setExitCodeCallback?.Invoke(memProcess.ExitCode);
+                lock (lockObject)
+                {
+                    Monitor.Pulse(lockObject);
+                }
+            };
+            memProcess.Run();
+        }
+    }
+}
