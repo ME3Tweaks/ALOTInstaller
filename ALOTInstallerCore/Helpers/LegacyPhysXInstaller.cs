a0e387157223cd51cac8a2c1918312eba83c5836
Mgamerz <mgamerz2@gmail.com>
2020-11-02 20:24:18 -0700

    Add PhysXLoader.dll patcher for ME1 no-admin mode

--- ALOTInstallerCore/Helpers/LegacyPhysXInstaller.cs
+++ ALOTInstallerCore/Helpers/LegacyPhysXInstaller.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ALOTInstallerCore.ModManager.ME3Tweaks;
+using ALOTInstallerCore.ModManager.Objects;
 using ME3ExplorerCore.Helpers;
 using Microsoft.Win32;
 using Serilog;
@@ -49,6 +50,46 @@ namespace ALOTInstallerCore.Helpers
             // Linux users should know what they're doing
             return true;
 #endif
+        }
+
+        /// <summary>
+        /// Patches the ME1 PhysxLoader to always use local. This will make it so ME1 will run regardless if it can access the registry or not with the enableLocalPhysXCore key.
+        /// </summary>
+        /// <param name="me1Target"></param>
+        /// <returns></returns>
+        public static bool PatchPhysXLoaderME1(GameTarget me1Target)
+        {
+            Log.Information("[AICORE] Patching PhysXLoader.dll to force loading local PhysXCore");
+            var loaderPath = Path.Combine(me1Target.TargetPath, "Binaries", "PhysXLoader.dll");
+            if (File.Exists(loaderPath) && new FileInfo(loaderPath).Length == 68688) //Make sure it's same size so it's not like some other build
+            {
+                using var pls = File.Open(loaderPath, FileMode.Open, FileAccess.ReadWrite);
+                pls.Seek(0x1688, SeekOrigin.Begin);
+
+                var jzByte1 = pls.ReadByte();
+                var jzByte2 = pls.ReadByte();
+                Log.Information($@"[AICORE] Byte 1 @ 0x1688: 0x{jzByte1:X2}");
+                Log.Information($@"[AICORE] Byte 2 @ 0x1689: 0x{jzByte2:X2}");
+                if (jzByte1 == 0x75 && jzByte2 == 0x19)
+                {
+                    // It's a jz instruction. Change to nop
+                    Log.Information(@"[AICORE] This file is has the original PhysXLoader.dll jump instruction for allowing system PhysX. Patching out to force local PhysX codepath");
+                    pls.Seek(-2, SeekOrigin.Current);
+                    pls.WriteByte(0x90); //nop
+                    pls.WriteByte(0x90); //nop
+                    Log.Information(@"[AICORE] PhysXLoader.dll has been patched");
+                }
+                else if (jzByte1 == 0x90 && jzByte2 == 0x90)
+                {
+                    Log.Information(@"[AICORE] This file appears to have already been patched to force local use of PhysX. Not patching file.");
+                }
+                else
+                {
+                    Log.Warning(@"[AICORE] Bytes are not expected values. We will not patch this file.");
+                    return false;
+                }
+            }
+            return true;
         }
 
         public static async Task<string> InstallLegacyPhysX(Action<long, long> processCallback, Action<string> setStatusCallback, Func<string, string, string, string, bool> confirmDialogCallback, Action<string, string> errorDialogCallback, CancellationTokenSource cancellationTokenSource)
