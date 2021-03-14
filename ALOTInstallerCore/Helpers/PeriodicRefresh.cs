af482d8b25c329b1a4cc3411a44aae062d3be641
Mgamerz <mgamerz2@gmail.com>
2021-03-14 15:50:47 -0600

    Add periodic refresh event handler

--- ALOTInstallerCore/Helpers/PeriodicRefresh.cs
+++ ALOTInstallerCore/Helpers/PeriodicRefresh.cs
@@ -13,7 +13,7 @@ namespace ALOTInstallerCore.Helpers
     public class PeriodicRefresh
     {
         private static Timer periodicTimer;
-
+        private static PeriodicRefresh pr;
         public static void StartPeriodicRefresh()
         {
             if (periodicTimer != null)
@@ -29,12 +29,19 @@ namespace ALOTInstallerCore.Helpers
             };
             periodicTimer.Elapsed += periodicRefresh;
             periodicTimer.Start();
+            pr = new PeriodicRefresh();
         }
 
         private static void periodicRefresh(object sender, ElapsedEventArgs e)
         {
             Debug.WriteLine("Periodic refresh");
             BackupService.RefreshBackupStatus(null, false, log: false);
+            OnPeriodicRefresh?.Invoke(null,null);
         }
+
+        /// <summary>
+        /// Invoked when a periodic refresh occurs, which happens every 60 seconds. The parameters from the call are always null.
+        /// </summary>
+        public static event EventHandler OnPeriodicRefresh;
     }
 }
