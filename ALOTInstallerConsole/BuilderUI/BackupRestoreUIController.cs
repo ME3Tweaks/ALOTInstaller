2b187ff9e8bb41391d628c4dceaa0627b49022c2
Mgamerz <mgamerz2@gmail.com>
2021-04-14 21:40:25 -0600

    Fix linux build

--- ALOTInstallerConsole/BuilderUI/BackupRestoreUIController.cs
+++ ALOTInstallerConsole/BuilderUI/BackupRestoreUIController.cs
@@ -9,6 +9,7 @@ using ALOTInstallerCore.ModManager.ME3Tweaks;
 using ALOTInstallerCore.ModManager.Objects;
 using ALOTInstallerCore.ModManager.Services;
 using ALOTInstallerCore.Objects;
+using ME3ExplorerCore.Gammtek.Extensions;
 using ME3ExplorerCore.Packages;
 using Terminal.Gui;
 
